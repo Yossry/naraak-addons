29434d8556d39d4f488f8b0480ffc175bdde8599
Your Name <you@example.com>
2021-09-10 21:20:18 +0300

    additonal Code in chart of account

---
+++ ak_additional_fields/__manifest__.py
@@ -0,0 +1,22 @@
+
+{
+    'name': "Additional fields for Naraak",
+    'version': "13.0.0.0",
+    'summary': "additional fields",
+    'category': 'Extra Addons',
+    'description': """
+        this app will add Account number field to chart of account
+    """,
+    'author': "Expertsintech",
+    'website':"www.expertsintech.com",
+    'depends': ['base', 'account'],
+    'data': [
+      'views/chart_of_account_view.xml',
+    ],
+    'demo': [],
+    "external_dependencies": {},
+    "license": "AGPL-3",
+    'installable': True,
+    'auto_install': False,
+
+}
